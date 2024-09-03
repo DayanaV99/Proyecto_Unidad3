7d7dd3828c889933edd6cbc54bacd9a4dbfa6009
Dayana <dayanavergarabravo@gmail.com>
2024-09-03 14:36:38 -0500

    Quinto commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio/inicio.component';
 import { ServicioComponent } from './servicio/servicio/servicio.component';
 import { ContactoComponent } from './contacto/contacto/contacto.component';
+import { MetodoPagoComponent } from './metodo-pago/metodo-pago.component';
 
 export const appRoutes: Routes = [
   { path: 'inicio', component: InicioComponent },
   { path: 'servicio', component: ServicioComponent },
   { path: 'contacto', component: ContactoComponent },
+  { path:'metodo-pago', component: MetodoPagoComponent },
   { path: '', redirectTo: '/inicio', pathMatch: 'full' }
+  
 ];
