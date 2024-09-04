5ead3cbdf87002deb37b21a03f4201f46d6d6d18
Dayana <dayanavergarabravo@gmail.com>
2024-09-03 23:17:09 -0500

    Finish commit

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,18 @@ import { appRoutes } from './app/app.routes';
 import { isDevMode } from '@angular/core';
 import { provideServiceWorker } from '@angular/service-worker';
 
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/ngsw-worker.js').then((registration) => {
+      console.log('Service Worker registrado con éxito:', registration);
+    }).catch((error) => {
+      console.error('Error en el registro del Service Worker:', error);
+    });
+  });
+}
+
+
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(appRoutes), provideServiceWorker('ngsw-worker.js', {
