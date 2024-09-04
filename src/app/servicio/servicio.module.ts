71efe723ac004794a4c77afced2ac02591c58059
NightCrawler927 <nelsonbarahona2011@gmail.com>
2024-09-03 21:08:53 -0500

    actualizacion pwa

--- src/app/servicio/servicio.module.ts
+++ src/app/servicio/servicio.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router'; // Importa RouterModule
-import { ServicioComponent } from './servicio/servicio.component'; // Importa el componente standalone
+
 
 @NgModule({
   // Elimina ServicioComponent de declarations, ya que es standalone
   imports: [
     CommonModule,
     RouterModule,
-    ServicioComponent // Importa el componente standalone aquí
+   
   ],
-  exports: [ServicioComponent]
+  exports: [CommonModule,RouterModule]
 })
 export class ServicioModule { }
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,17 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { appRoutes } from './app/app.routes';
+import { isDevMode } from '@angular/core';
+import { provideServiceWorker } from '@angular/service-worker';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(appRoutes)
+    provideRouter(appRoutes), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
   ]
 }).catch(err => console.error(err));
